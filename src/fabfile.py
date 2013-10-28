b9a351b9bf4f27c4791de390302c01b4b732928d
SatoShun <shun.sato1@gmail.com>
2013-10-28 16:10:20 +0900

    target all

--- src/fabfile.py
+++ src/fabfile.py
@@ -1,10 +1,23 @@
 from fabric.api import local, task
+from fabric.colors import blue
 
 
 @task
-def install():
-    local('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"')
-    local('brew update')
-    local('brew install apple-gcc42  bash-completion docbook-xsl gettext go imagemagick libmemcached lv msgpack node phantomjs python3 readline scons tig xz '
-          'asciidoc cmake freetype git gradle jpeg libpng memcached mysql openssl pkg-config qt redis shiboken tree '
-          'autoconf docbook gdbm gnu-getopt groonga libevent libtool mongodb nginx pcre python rbenv ruby-build sqlite xmlto')
+def install(target='all'):
+    def homebrew():
+        local('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"')
+        local('brew update')
+        local('brew install apple-gcc42  bash-completion docbook-xsl gettext go imagemagick libmemcached lv msgpack node phantomjs python3 readline scons tig xz '
+              'asciidoc cmake freetype git gradle jpeg libpng memcached mysql openssl pkg-config qt redis shiboken tree '
+              'autoconf docbook gdbm gnu-getopt groonga libevent libtool mongodb nginx pcre python rbenv ruby-build sqlite xmlto')
+
+    def python():
+        local('sudo easy_install pip')
+        local('sudo pip install virtualenv virtualenvwrapper')
+        local('sudo pip install sphinx fabric')
+
+    if target == 'all':
+        for name, func in locals():
+            if callable(func):
+                print(blue('call: ' + name))
+                func()
