aea37c3c8c6223ae9e3f8617be2f7910564a731b
SatoShun <shun.sato1@gmail.com>
2013-10-28 14:57:35 +0900

    first

---
+++ src/fabfile.py
@@ -0,0 +1,9 @@
+from fabric import local, tasks
+
+
+@tasks
+def install():
+    local('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"')
+    local('brew install apple-gcc42  bash-completion docbook-xsl gettext go imagemagick libmemcached lv msgpack node phantomjs python3 readline scons tig xz '
+          'asciidoc cmake freetype git gradle jpeg libpng memcached mysql openssl pkg-config qt redis shiboken tree '
+          'autoconf docbook gdbm gnu-getopt groonga libevent libtool mongodb nginx pcre python rbenv ruby-build sqlite xmlto')
