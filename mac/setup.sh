46f7381900b53a2dd9f6fa7e606f682cede49b0d
SatoShun <shun.sato1@gmail.com>
2013-10-29 14:12:24 +0900

    Fixed path

--- mac/setup.sh
+++ mac/setup.sh
@@ -5,10 +5,10 @@ sublime()
     sublimeDir=~/Library/Application\ Support/Sublime\ Text\ 3
     symSublimeDir=~/SkyDrive/preference/Sublime\ Text\ 3
 
-    rm -rf "${sublimeDir}/Installed\ Packages"
+    rm -rf "${sublimeDir}/Installed Packages"
     rm -rf "${sublimeDir}/Packages"
 
-    ln -s "${symSublimeDir}/Installed\ Packages" "${sublimeDir}/Installed\ Packages"
+    ln -s "${symSublimeDir}/Installed Packages" "${sublimeDir}/Installed Packages"
     ln -s "${symSublimeDir}/Packages" "${sublimeDir}/Packages"
 }
 
