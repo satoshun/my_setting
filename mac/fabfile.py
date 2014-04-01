0f58f97545a0e46e0093ac5f841a2f33d3e5192c
SatoShun <shun.sato1@gmail.com>
2014-04-02 01:16:54 +0900

    add brewfile

--- mac/fabfile.py
+++ mac/fabfile.py
@@ -14,20 +14,6 @@ def is_command_exist(command):
 
 @task
 def install(target='all'):
-    def homebrew():
-        if not is_command_exist('brew'):
-            local('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"')
-            local('brew update')
-
-            for item in ['bash-completion', 'go', 'imagemagick', 'libmemcached',
-                         'lv', 'node', 'python3', 'readline', 'tig', 'xz',
-                         'git', 'gradle', 'jpeg', 'memcached', 'mysql', 'redis', 'tree',
-                         'autoconf', 'groonga', 'mongodb', 'nginx', 'rbenv', 'ruby-build', 'sqlite', 'phantomjs', 'qt']:
-                local('brew install {0}'.format(item))
-                local('sudo pip-3.3 install virtualenv virtualenvwrapper')
-
-            local('brew install "http://toolbelt.treasure-data.com/brew/td-agent.rb"')
-
     def rbenv():
         local('rbenv install 2.0.0-p247')
         local('rbenv rehash')
@@ -47,17 +33,6 @@ def install(target='all'):
 
     def my_setting():
         local('git clone git@github.com:satoshun/utility.git {0}/utility'.format(git_dir))
-
-    def dmg():
-        # https://github.com/phinze/homebrew-cask
-        local('brew tap phinze/cask')
-        local('brew install brew-cask')
-        for package in [
-            'google-chrome', 'dropbox', 'skydrive', 'skype', 'sourcetree',
-            'android-studio', 'hipchat', 'virtualbox', 'vagrant', 'xtrafinder',
-            'iterm2', 'java', 'alfred', 'dash', 'evernote', 'firefox', 'bettertouchtool',
-            'keyremap4macbook', 'google-web-designer']:
-            local('brew cask install {0}'.format(package))
 
     if target == 'all':
         for name, func in locals().items():
