12f20a7dae4d7014a8b13e3e3582d68c30aa2260
SatoShun <shun.sato1@gmail.com>
2015-12-14 13:12:09 +0000

    dotfiles

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -1,51 +1,7 @@
 #!/bin/sh
 
-SYMLINK_LIST="
-agignore
-bash_common
-bash_profile
-bashrc
-codeintel/config
-gitconfig
-gitignore_global
-inputrc
-mongorc.js
-my.cnf
-shalias
-tmuxinator
-shfunc
-tern-project
-tigrc
-zaw
-zshrc
-zshenv
-tmux.conf
-csscomb.json
-"
-
-# .jshintrc
-# .gradle
-
-mkdir -p ~/.codeintel
-DOTFILES_PATH=$(cd $(dirname $0);pwd)
-
-for file in ${SYMLINK_LIST};
-do
-    ln -sf ${DOTFILES_PATH}/${file} ~/.${file}
-done
-
 # mkdir -p ~/bin
 # sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin
-
-# zsh
-# curl -L http://install.ohmyz.sh | sh
-
-# curl -sSL https://get.rvm.io | bash
-
-# curl -o diff-highlight https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight
-
-# npm install -g jshint csslint
-# sudo easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz
 
 ## ricty
 # cp -f /usr/local/Cellar/ricty/3.2.3/share/fonts/Ricty*.ttf ~/Library/Fonts/
@@ -54,17 +10,3 @@ done
 ## digital ocean
 # vagrant plugin install dotenv
 # vagrant plugin install vagrant-digitalocean
-# vagrant plugin install dotenv
-
-## go
-# go get github.com/golang/lint/golint
-# hash -r
-# go get code.google.com/p/go.tools/cmd/goimports
-
-## node
-# npm install -g gulp typescript yo compass browserify jshint
-
-## typescript
-# npm install -g typescript
-# npm install -g tslint
-# npm install -g generator-typescript
